5382d67eff1efc9ee8b7eef6c5bab0f475079f0b
tythomassen <tyler.thomassen@icloud.com>
2026-01-23 16:37:51 -0500

    initial commit

---
+++ Data_Logs.py
@@ -0,0 +1,3 @@
+
+SIS_COURSES_LINK = "https://sis.rutgers.edu/soc/api/courses.json?year=2024&term=1&campus=NB"
+SIS_OPEN_COURSES_LINK = "https://sis.rutgers.edu/soc/api/openSections.json?year=2024&term=1&campus=NB"
